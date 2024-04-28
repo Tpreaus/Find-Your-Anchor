a5df43e2c4ee47fbedb490a0acade563cd6b2075
beachlettuce <142933138+beachlettuce@users.noreply.github.com>
2024-04-28 15:37:49 -0400

    comments
    comment

--- backend/models/club.js
+++ backend/models/club.js
@@ -1,21 +1,24 @@
+//mongoose library
 const mongoose = require('mongoose');
 
+// Defining the schema for Rollins Club
 const rollinsClubSchema = new mongoose.Schema({
   "Activity Name": {
     type: String,
-    required: true // Assuming the Activity Name is required
+    required: true 
   },
   "Description": {
     type: String,
-    required: true // Assuming the Description is also required
+    required: true 
   },
-  "Image URL": {  // Adding the image URL field
+  "Image URL": {  
     type: String,
-    required: true  // Assuming the image URL might not be required
+    required: true  
   }
-  // You can add other fields here as needed, following the structure of your documents
 });
 
+// Creating the RollinsClub model using the schema
 const RollinsClub = mongoose.model('RollinsClub', rollinsClubSchema, 'rollinsClubs');
 
+// Exporting the RollinsClub model
 module.exports = RollinsClub;
